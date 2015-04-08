abd8c63e01d7b937fb69ca21eb483827733d978b
AnitaBayrash <anitikki@list.ru>
2015-04-08 14:48:18 +0300

    New timer test added.

--- timer.sh
+++ timer.sh
@@ -1,8 +1,15 @@
 make >> logger.txt
 sudo insmod ./timer.ko
+
 echo "Test: timer_period = 0.5 sec.:"
 echo 500 >> /sys/timer/timer_period
 sleep 2
-dmesg | tail -n 10
+dmesg | tail -n 5
+
+echo "Test: timer_period = 1 sec.:"
+echo 1000 >> /sys/timer/timer_period
+sleep 3
+dmesg | tail -n 5
+
 sudo rmmod timer
 make clean >> logger.txt
