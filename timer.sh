f0d490a04c893fafabe7756ecce813a7ed163600
AnitaBayrash <anitikki@list.ru>
2015-04-08 10:54:03 +0300

    Shell file with timer test added.

--- timer.sh
+++ timer.sh
@@ -1,6 +1,8 @@
 make >> logger.txt
 sudo insmod ./timer.ko
+echo "Test: timer_period = 0.5 sec.:"
 echo 500 >> /sys/timer/timer_period
-cat /sys/timer/timer_period
+sleep 2
+dmesg | tail -n 10
 sudo rmmod timer
 make clean >> logger.txt
